41224eba749f0fd371cff53176c4592820052766
Sayali Jagtap <jagtapsayali0304@gmail.com>
2018-07-31 17:49:50 +0530

    tests added for add functionality

--- test/addTest.js
+++ test/addTest.js
@@ -2,7 +2,11 @@ const assert=require("chai").assert;
 const add=require("../src/basic.js").add;
 
 describe("add test", function() {
-  it("should add 2 numbers", function() {
+  it("should add 10 and 21 numbers", function() {
     assert.equal(add(10,21), 31);
+  });
+
+  it("should add -2 and 66 numbers", function() {
+    assert.equal(add(-2,66), 64);
   });
 });
